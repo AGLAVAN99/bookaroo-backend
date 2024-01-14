cab61eb1d356048c7a0cd7829f92811b73553805
Andreea Glavan <andreea.glavan999@yahoo.com>
2024-01-14 18:54:01 +0200

    Initial commit

---
+++ src/main/java/com/bookaroo/bookaroo/BookController.java
@@ -0,0 +1,9 @@
+package com.bookaroo.bookaroo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BookController {
+}
